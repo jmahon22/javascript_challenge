ec2434df4d195ec66914622f5ac8fadbd5690905
Jen Mahon <Jen.Mahon@villageinn.com>
2020-12-28 10:17:56 -0700

    ufo level 1 finished

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -15,7 +15,33 @@ tableData.forEach((ufoInfo) => {
 
 // Use a date form in your HTML document and write JavaScript code that will listen for events 
 //and search through the `date/time` column to find rows that match user input.
-// Select the button
+// Select button
 var button = d3.select("#filter-btn");
-// Select the form
+// Select form
 var form = d3.select("#form");
+
+// Event handler function and return form
+button.on("click", function() {
+    // Clear table body
+    tbody.html("");
+    //Prevent page from refreshing
+    d3.event.preventDefault();
+    //Select the input element
+    var inputElement = d3.select("#datetime");
+    //Get the value property of input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    //Filter data
+    var matches = tableData.filter(input => input.datetime === inputValue);
+    console.log(matches);
+    // Append data that matches datetime input to table, so table only shows data that matches users input
+    matches.forEach((ufoInput) => {
+        console.log(ufoInput);
+        var row = tbody.append("tr");
+        Object.entries(ufoInput).forEach(([key, value])=> {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
+});
